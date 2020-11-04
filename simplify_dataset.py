Normalization fixed.

simplify_dataset.py
@@ -37,6 +37,7 @@ if __name__ == '__main__':
 
 
     data_folder = '/home/ervin/Dokumentumok/brc/Adipocyte/sample'
+    data_folder = '/media/ervin/SSD-Data/adipocyte/data'
     out_path = 'out'
 
     """
@@ -69,7 +70,8 @@ if __name__ == '__main__':
         for idx, k in enumerate(ims.keys()):
             merged = []
             for ch_id, im in enumerate(ims[k][:3]):
-                ch = imageio.imread(os.path.join(data_folder, magnification, im))
+                im_path = os.path.join(data_folder, magnification, im)
+                ch = imageio.imread(im_path)
                 
                 sort_ch = np.sort(np.reshape(ch, (-1,)))
                 n = len(sort_ch)
@@ -79,9 +81,11 @@ if __name__ == '__main__':
 
                 low[ch_id].append(a)
                 high[ch_id].append(b)
-            
+                print(im_path, a, b)
+
             for b in ims[k][3:]:
-                b_im = imageio.imread(os.path.join(data_folder, magnification, b))
+                im_path = os.path.join(data_folder, magnification, b)
+                b_im = imageio.imread(im_path)
 
                 sort_ch = np.sort(np.reshape(b_im, (-1,)))
                 n = len(sort_ch)
@@ -92,7 +96,7 @@ if __name__ == '__main__':
                 ch_id = 3
                 low[ch_id].append(a)
                 high[ch_id].append(b)
-
+                print(im_path, a, b)
         low_med, high_med = get_med_ch(low, high)
         print('Computed histo stretch limit (med) for each channel:', low_med, high_med)
 

train.py
@@ -106,21 +106,13 @@ class AZSequence(Sequence):
     def __getitem__(self, idx):
         def normalize(im, low, high):
             """ Threshold and divide """
-            s = np.sort(np.reshape(im, (-1,)))
-            l = len(im)
-            p = round(l/100)
-            print('Normalizing: threshold:', high, 'max:', np.max(im), '100th', s[p], '-100th', s[-p])
+            histo = False
+            if histo:
+                im[im > high] = high
+                im = im / high
+            else:
+                im /= 65535
 
-            im[im > high] = high
-            im[im < low] = low
-
-            im = im - low
-            im = im / (high-low)
-
-            #im = im / high
-
-            plt.imshow(im)
-            plt.show()
             return im
 
         image_idx = idx // self.sample_per_image
@@ -433,7 +425,7 @@ def test(sequence, model=None, save=False, tile_sizes=None):
 
             y_pred_sample_normalized = np.zeros_like(y_pred_sample)
             for ch in range(np.shape(y_pred_sample)[-1]):
-                y_pred_sample_normalized[..., ch] = y_pred_sample[..., ch] / np.max(y_pred_sample[..., ch])
+                y_pred_sample_normalized[..., ch] = y_pred_sample[..., ch] / 65535
 
             plt.subplot(plot_layout + 6, title='Predicted fluorescent (red)')
             plt.imshow(y_pred_sample[..., 0])