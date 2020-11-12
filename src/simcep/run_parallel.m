4880bf8555cf6fb97d096a1228f174658df967b1
Krisztian Koos <koos.krisztian@gmail.com>
2020-11-13 00:26:01 +0100

    update simcep generation

--- src/simcep/run_parallel.m
+++ src/simcep/run_parallel.m
@@ -1,4 +1,4 @@
-n_images = 1000;
+n_images = 10;
 magnification = 60;
 output_dir = '/home/koosk/data/images/adipocyte/generated_masks/';
 
@@ -6,7 +6,9 @@ copyfile(['simcep_options', num2str(magnification), '.m'], 'simcep_options.m')
 
 parfor i = 1:n_images
     [~, binary] = simcep;
-    fname = ['generated_nuclei_mask_', num2str(magnification), 'x-', datestr(now, 'YYYY-mm-DD_HHMMssFFF'), '.png'];
+    random_id = char(java.util.UUID.randomUUID);
+    random_id = random_id(1:8);
+    fname = ['generated_nuclei_mask_', num2str(magnification), 'x-', datestr(now, 'YYYY-mm-DD_HHMMssFFF'), '-', random_id, '.png'];
     fpath = fullfile(output_dir, fname);
     imwrite(binary, fpath);
 end
